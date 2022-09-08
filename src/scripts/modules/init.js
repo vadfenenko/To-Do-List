f51718f4a3d7a3e6790f2a7539995a6d15cc1340
unknown <vadfenenko@gmail.com>
2022-09-08 10:57:54 +0200

    fixed modul init.js

--- src/scripts/modules/init.js
+++ src/scripts/modules/init.js
@@ -1,21 +1,25 @@
 import {database} from './database.js';
 import {process} from './process.js';
 
-if(localStorage.length === 0) {
-    widnow.localStorage.setItem('active-tasks', '[]');
-    window.localStorage.setItem('complete-tasks', '[]');
-}
+    if(window.localStorage.length === 0) {
+        window.localStorage.setItem('active-tasks', '[]');
+        window.localStorage.setItem('complete-tasks', '[]');
+    }
+    
+    const tasksComplete = database.getData('complete-tasks')
+    
+    if(tasksComplete.length > 1000) { //memory limits
+        tasksComplete.splice(0, 500)
+        database.setData(tasksComplete, 'complete-tasks')
+    }
+      
+    window.tasksActive = true;
+    const activeTasks = database.getData('active-tasks');
+    process.render(activeTasks);
 
-const tasksComplete = database.getData('complete-tasks')
 
-if(tasksComplete.length > 1000) { //memory limits
-    tasksComplete.splice(0, 500)
-    database.setData(tasksComplete, 'complete-tasks')
-}
-  
-window.tasksActive = true;
-const activeTasks = database.getData('active-tasks');
-process.render(activeTasks);
+
+
 
 
 
